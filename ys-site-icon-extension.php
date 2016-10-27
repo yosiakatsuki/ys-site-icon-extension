d75a4a523806be3b5d187efb13beeee13812eadc
Yoshiaki Ogata <yosi@yosi.local>
2016-10-27 20:48:11 +0900

    first commit

---
+++ ys-site-icon-extension.php
@@ -0,0 +1,14 @@
+<?php
+/*
+	Plugin Name: ys Site Icon Extension
+	Plugin URI: https://github.com/yosiakatsuki/ys-site-icon-extension
+	Description: テーマカスタマイザーの「サイトアイコン」設定を拡張するプラグイン
+	Version: 1.0
+	Author: Yoshiaki Ogata
+	Author URI:
+	Created : October 28, 2016
+	Modified: October 28, 2016
+	License: GPLv2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+?>
